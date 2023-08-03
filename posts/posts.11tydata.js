c46ee535b8ff147ce9fd9f9321c13e890175ff66
Vince Aggrippino <195491+VAggrippino@users.noreply.github.com>
2023-08-03 16:14:08 +0800

    Remove irrelevant bits from configuration

--- posts/posts.11tydata.js
+++ posts/posts.11tydata.js
@@ -1,12 +1,4 @@
-const dayjs = require('dayjs')
-
 module.exports = () => ({
   "layout": "base.njk",
   "tags": ["post"],
-  // Write output files without their own permalink to a path that includes the year and month
-  "permalink": data => `/${dayjs(data.page.date).format('YYYY/MM')}/${data.page.fileSlug}/`,
-
-  "eleventyComputed": {
-    "eleventyExcludeFromCollections": data => !data.published,
-  },
 })
